6bbbd9336a4aa235e6e664db1b5e53c87e643110
thibaud aptel <thibaudaptel@thibauds-MBP.modem>
2023-09-25 14:20:56 +1000

    use VITE_ENV for all the API variables

--- src/Api/TasksAPI.ts
+++ src/Api/TasksAPI.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = import.meta.env.VITE_REACT_APP_BASE_URL;
 
 // Tasks ----------
 export const fetchTasks = async (cardId: string) => {
--- src/Api/TasksDueTodayAPI.ts
+++ src/Api/TasksDueTodayAPI.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = import.meta.env.VITE_REACT_APP_BASE_URL;
 
 // Tasks Due Today ----------
 export const fetchTasksDueToday = async () => {
