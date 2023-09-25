9900e1b42a1c1f3ac0afb00ade9bb9189cc01c6f
thibaud aptel <thibaudaptel@thibauds-MBP.modem>
2023-09-25 11:23:10 +1000

    added API endpoint ProjectsAPI

--- src/Api/ProjectsAPI.ts
+++ src/Api/ProjectsAPI.ts
@@ -1,14 +1,16 @@
+const BASE_URL = "https://journal-backend-5pfe.onrender.com";
+
 // Projects ----------
 export const fetchProjects = () =>
-  fetch("http://localhost:3001/projects").then((res) => res.json());
+  fetch(`${BASE_URL}/projects`).then((res) => res.json());
 
 export const deleteProject = (id: string) =>
-  fetch(`http://localhost:3001/projects/${id}`, { method: "DELETE" }).then(
-    (res) => res.json()
+  fetch(`${BASE_URL}/projects/${id}`, { method: "DELETE" }).then((res) =>
+    res.json()
   );
 
 export const postProject = (title: string) =>
-  fetch("http://localhost:3001/projects", {
+  fetch(`${BASE_URL}/projects`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ title }),
