c79f2af0f3e75dd7b6097835072c97fbe5389acb
thibaud aptel <thibaudaptel@thibauds-MBP.modem>
2023-10-30 12:40:49 +1100

    fix error oin taskSlice

--- src/Api/index.ts
+++ src/Api/index.ts
@@ -1,6 +1,5 @@
 export * from "./TopicsAPI";
 export * from "./PhotosAPI";
 export * from "./TasksAPI";
-export * from "./TasksDueTodayAPI";
 export * from "./SignUpAPI";
 export * from "./ProjectsAPI";
